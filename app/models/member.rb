299e52aeb63f48d3e13d6efc4d2b52164efb3b3d
Burak Çolak <kilitbilgi@gmail.com>
2014-12-07 22:18:44 +0200

    commit for adding css

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,9 @@
 class Member < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
 end
