ba9db106fce258764743228c5b5158f7d18b600b
Burak Çolak <kilitbilgi@gmail.com>
2014-12-08 23:40:38 +0200

    commit for adding views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,8 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :none
   # config.assets.css_compressor = :sass
-
-  config.assets.compress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
