65deee3a75499045dd51b4dbf73bb96d92490e99
Burak Çolak <kilitbilgi@gmail.com>
2014-12-12 23:06:49 +0200

    commit for login

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -28,11 +28,12 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.save
-        format.html { redirect_to @member, notice: 'Member was successfully created.' }
-        format.json { render :show, status: :created, location: @member }
+        #format.html { redirect_to @member, notice: 'Member was successfully created.' }
+        redirect_to root_path
+        #format.json { render :show, status: :created, location: @member }
       else
-        format.html { render :new }
-        format.json { render json: @member.errors, status: :unprocessable_entity }
+        redirect_to root_path
+        #format.json { render json: @member.errors, status: :unprocessable_entity }
       end
     end
   end
